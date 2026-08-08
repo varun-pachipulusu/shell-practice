0f29a82e5f67b7f6ac7ad7a22773f62d37153748
Your Name <varunsai2246090@gmail.com>
2026-08-08 18:02:58 +0530

    changes

--- 05-variables-DT.sh
+++ 05-variables-DT.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+start_time=$(date +%s)
 
-echo "script executed at: $TIMESTAMP"
+echo "script executed at: $start_time"
+
+sleep 20
+
+end_time=$(date +%s)
+
+
+echo "script finished at: $end_time"
+
+TOTAL_TIME=$((end_time - start_time))

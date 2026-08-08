36e86e565a863df3d09fcf2db4935bed67c2e7d3
Your Name <varunsai2246090@gmail.com>
2026-08-08 17:30:15 +0530

    changes

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1="$1"
+PERSON2="$2"
+
+echo "${PERSON1}: Hey ${PERSON2}, are you free this evening?"
+echo "${PERSON2}: Yeah, I’m free. What’s up?"
+echo "${PERSON1}: Let’s go out for some coffee."
+echo "${PERSON2}: Sounds good! What time?"
+echo "${PERSON1}: Around 6 PM. See you then!"
+

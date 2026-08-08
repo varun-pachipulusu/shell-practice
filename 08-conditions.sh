a00bf8b1b268215231e24d3f417c6dd562dd0177
Your Name <varunsai2246090@gmail.com>
2026-08-08 19:27:21 +0530

    changes

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,25 +1,26 @@
-#syntax for if statement in bash shell scripting
-if [ expression ]; then
-	code here
-fi
-
-if [ expression ]; then
-	code here
-elif [ expression ]; then
-	code here
-else
-	code here
-fi
-
 #!/bin/bash
+
+# syntax for if statement in bash shell scripting
+# if [ expression ]; then
+#     code here
+# fi
+
+# if [ expression ]; then
+#     code here
+# elif [ expression ]; then
+#     code here
+# else
+#     code here
+# fi
+
 
 NUMBER=$1
 
-#-gt --> greater than
-#-lt --> less than
-#-eq --> equal to
-#-n --> not equal to
+# -gt --> greater than
+# -lt --> less than
+# -eq --> equal to
+# -n --> not equal to
 
-if [ $NUMBER -gt 40 ]; then
-    echo "the given number:$NUMBER is greater than 40"
-fi   
+if [ "$NUMBER" -gt 40 ]; then
+    echo "The given number: $NUMBER is greater than 40"
+fi

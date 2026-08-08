cfb4b867ed404c2b3aa2ef4ef0bc6eb14dc0c3f5
Your Name <varunsai2246090@gmail.com>
2026-08-08 17:44:02 +0530

    changes

--- 04-variables-s.sh
+++ 04-variables-s.sh
@@ -1,9 +1,9 @@
-echo 'please enter your user name'
+echo 'please enter your user name::'
 read user_name
 
 echo "User name is $user_name"
 
-echo "please enter your password"
+echo "please enter your password::"
 read-s password
 
 echo "Password is $password"

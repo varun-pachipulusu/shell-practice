58475061cf741eee35f09dabc5174f9ac4f39710
Your Name <varunsai2246090@gmail.com>
2026-08-08 19:09:20 +0530

    changes

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -6,7 +6,10 @@ sum=$((num1 + num2))
 echo "The sum of $num1 and $num2 is: $sum"
 
 
-#Array
+#Array - list of values
 fruits=("apple" "banana" "cherry")
 
 echo "fruits are: ${fruits[@]}"
+echo "First fruit is: ${fruits[0]}"
+echo "Second fruit is: ${fruits[1]}"
+echo "Third fruit is: ${fruits[2]}"

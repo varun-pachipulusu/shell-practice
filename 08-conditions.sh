92fcf40cc82bb701f12e88fef4fcc051fc5f9aae
Your Name <varunsai2246090@gmail.com>
2026-08-08 19:39:36 +0530

    changes

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -23,7 +23,7 @@ NUMBER=$1
 
 if [ "$NUMBER" -gt 40 ]; then
     echo "The given number: $NUMBER is greater than 40"
-    elif ["$NUMBER" -eq 40]; then
+    elif [ "$NUMBER" -eq 40 ]; then
     echo "The given number: $NUMBER is equal to 40"
    else
     echo "The given number: $NUMBER is less than  40"

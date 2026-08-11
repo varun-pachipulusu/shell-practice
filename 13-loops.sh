27357174b0923f7c3afc83f43eeac299ce8f1b24
Your Name <varunsai2246090@gmail.com>
2026-08-12 05:15:01 +0530

    changes

---
+++ 13-loops.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+
+ echo "please run the script with root user access" | tee -a $LOGS_FILE
+ exit 1
+
+fi
+
+#by default shell will not execute,only executed when called
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo "$2 is failled" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo "$2 is success" | tee -a $LOGS_FILE
+
+fi
+
+}
+
+for package in $@
+do
+    dnf install $package -y
+    VALIDATE $? "$package installation"
+done
